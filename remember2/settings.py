0718084f768230eed5fba1ed1f8daf6640118de2
Jamie Bliss <jamie@ivyleav.es>
2020-05-07 16:32:01 -0400

    Enable logging

--- remember2/settings.py
+++ remember2/settings.py
@@ -126,3 +126,18 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+    },
+}
