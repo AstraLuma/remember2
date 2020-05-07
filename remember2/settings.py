1a4baf744f8fa9bf24b92c3f7d7ccc2057752ee7
Jamie Bliss <jamie@ivyleav.es>
2020-05-07 14:19:39 -0400

    Turn on debugging for now

--- remember2/settings.py
+++ remember2/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY', 'DEVELOPMENT_ONLY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = ('DYNO' not in os.environ)
+# DEBUG = ('DYNO' not in os.environ)
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
